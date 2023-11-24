a1e54d1f4a078f8e445ca987dcee148e514d1ca3
Tomas <tomasmolly@hotmail.com>
2023-11-24 17:32:01 -0300

    upgrade

--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 relacionada com a frase. Além de ter a opção de cadastrar no banco de dados frases, sentimentos, imagens e/ou gif. As consultas são exportadas 
 em formato json.
 
-# Instalação da aplicação
-node
-npm install
+# Instalação da aplicação 
+1. Clonar o repositorio do github neste endereço: "https://github.com/tomasrzdevgold/Api-FrasesMotivacionas.git"
+2. Ter instalado Node.js
+3. Inicializar a aplicação pela terminal com o comando "npm install",isso instalara todas as dependencias nesccesarias para usar a api
+4. Por ultimo para rodar o servidor é só digitar o comando no terminal "npm start"
 
-# Como utilizar a API
+# Como utilizar a API com interface gráfica
 
-1. Primeiro paso entrar no endereço da api, vai ser mostrado um simples formulario onde é possivel pôr:
+1. Primeiro paso abrir o arquivo index.html, vai ser mostrado um simples formulario onde é possivel pôr:
    - Qual tipo de frase motivacional você quer ligada pelo sentimento
    - Quantidade de frases que você quer receber
 
 2. Clicando na opção de cadastro você será redirecionado para a tela de cadastro da api, onde comtém as seguintes opções:
-   - Opção de cadastrar um novo sentimento que não enteja no banco de dados ou colocar um sentimento que já existe no banco de dados para logo
-        só cadastrar uma frase
-   - Opção de cadastar uma nova frase, simplesmente é colocar a frase sem aspas no input colocar frase
+   - Opção de cadastrar um novo sentimento que não enteja no banco de dados ou colocar um sentimento que já existe no banco de dados para logo só cadastrar uma frase
+   - Opção de cadastar uma nova frase, simplesmente é colocar a frase sem aspas no input 'colocar frase'
--- api.js
+++ api.js
@@ -45,22 +45,108 @@ srv.post('/frase',(req,res)=>{
 
 srv.post('/frase',(req,res)=>{
     const valor = req.body.tipo
+    const quantia = req.body.quantia
+    let aleatorio = parseInt( Math.random()*2)
     if(valor === "alegria"){
+        const array = ['https://th.bing.com/th?id=OIP.rt2lgAyhKvp9X5eN6qLrnAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2','https://i1.wp.com/destilos.com.br/wp-content/uploads/2018/12/alegria.jpeg?ssl=1']
         let db = new sqlite3.Database('frases.db')
-        let sql = `SELECT frase_motivacional FROM frases WHERE sentimentoID = 1;`
-        db.all(sql,[],(erro,linha)=>{
+        let sql = `SELECT frase_motivacional FROM frases WHERE sentimentoID = 1 LIMIT ?;`
+        db.all(sql,[quantia],(erro,linha)=>{
             if (erro) throw erro
-            res.json(linha)
+               
+
+            let resposta = `
+
+            <table style ="
+                border: 1px solid black
+            "
+            
+         >
+            <thead style ="
+            border: 1px solid black
+        ">
+                <tr style ="
+                border: 1px solid black
+            ">
+                    <th style ="
+                    border: 1px solid black
+                "> Frase </th>
+                  
+                </tr>
+            `
+            for (let index = 0; index < linha.length; index++) {
+                const element = linha[index]
+                const to = element["frase_motivacional"]
+                resposta += `
+   
+        <tr>
+        <td style ="
+        border: 1px solid black
+    "> ${to} </td>
+    </tr>
+
+                `
+            }
+            resposta += `    </thead>
+            </table>`
+
+            
+            resposta += `<br> <img src="${array[aleatorio]}" height="400">`
+            
+            res.send(resposta)
         })
 
 
         db.close()
     }else if (valor === "raiva"){
+        const array = ['https://th.bing.com/th?id=OIP.BxKGjtSD4Bi6S1qVHZt08gHaEy&w=311&h=200&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2','https://th.bing.com/th?id=OIP.pxQekMUL6VgbDFdTR4FSQgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2']
         let db = new sqlite3.Database('frases.db')
         let sql = `SELECT frase_motivacional FROM frases WHERE sentimentoID = 2 LIMIT ?;`
         db.all(sql,[quantia],(erro,linha)=>{
             if(erro) throw erro
-            res.json(linha)
+
+        
+                
+
+            let resposta = `
+
+            <table style ="
+                border: 1px solid black
+            "
+            
+         >
+            <thead style ="
+            border: 1px solid black
+        ">
+                <tr style ="
+                border: 1px solid black
+            ">
+                    <th style ="
+                    border: 1px solid black
+                "> Frase </th>
+                  
+                </tr>
+            `
+            for (let index = 0; index < linha.length; index++) {
+                const element = linha[index]
+                const to = element["frase_motivacional"]
+                resposta += `
+   
+        <tr>
+        <td style ="
+        border: 1px solid black
+    "> ${to} </td>
+    </tr>
+
+                `
+            }
+            resposta += `    </thead>
+            </table>`
+
+            
+            resposta += `<br> <img src="${array[aleatorio]}" height="400">`
+            
+            res.send(resposta)
         
     })
         db.close()
@@ -109,23 +195,40 @@ srv.post('/cadastro',(req,res)=>{
         let db = new sqlite3.Database('frases.db')
         let sql = `INSERT INTO frases (frase_motivacional, sentimentoID) VALUES (?,?);`
         db.get(sql,[frase,valor],(erro,linha)=>{
-            if(erro) throw erro
+           // if(erro) throw erro
 
             res.send(`<!DOCTYPE html>
             <html lang="en">
             <body>
-                <h1>CASTRO COM SUCESSO</h1>
+                <h1>CADASTRO COM SUCESSO</h1>
+                <a href="http://127.0.0.1:5500/cadastro.html">Cadastra novas frases</a>
             </body>
             </html>
             `)
         })
         db.close()
-    }else {
+    }
+     else if(valor === "2"){
+        let db = new sqlite3.Database('frases.db')
+        let sql = `INSERT INTO frases (frase_motivacional, sentimentoID) VALUES (?,?);`
+        db.get(sql,[frase,valor],(erro,linha)=>{
+           // if(erro) throw erro
+
+            res.send(`<!DOCTYPE html>
+            <html lang="en">
+            <body>
+                <h1>CADASTRO COM SUCESSO</h1>
+                <a href="http://127.0.0.1:5500/cadastro.html">Cadastra novas frases</a>
+            </body>
+            </html>
+            `)
+        })
+        db.close()
+    } else {
         res.send(`
-            <p>de algum erro</p> <a href="http://127.0.0.1:5500/cadastro.html">Cadastra novas frases</a>
+            <p>deu algum erro</p> <a href="http://127.0.0.1:5500/cadastro.html">Cadastra novas frases</a>
         `)
     }
-  
 })
 
 
